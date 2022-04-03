1acabb669abb584e158a5da6449b34f8eca9912f
Javier Piqueras <javipiquerasm@gmail.com>
2022-04-03 13:02:21 +0200

    Module structs

--- evenorodd/main.go
+++ evenorodd/main.go
@@ -6,9 +6,9 @@ func main() {
 	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, val := range a {
 		if val%2 == 0 {
-			fmt.Println(val, " is even")
+			fmt.Println(val, "is even")
 		} else {
-			fmt.Println(val, " is odd")
+			fmt.Println(val, "is odd")
 		}
 	}
 }
