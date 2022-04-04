9b9c4903d8958d860d3324b28ab83be68142a27d
Javier Piqueras <javipiquerasm@gmail.com>
2022-04-04 22:38:12 +0200

    Interfaces assignment two - done

--- interfaces-assignment-two/main.go
+++ interfaces-assignment-two/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	fc, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, fc)
 }
